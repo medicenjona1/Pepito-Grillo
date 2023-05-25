20d9a4859ad376b4d48758871e3144cdbd175a9d
medicenjona1 <89283235+medicenjona1@users.noreply.github.com>
2023-05-25 19:26:05 +0000

    Pending changes exported from your codespace

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const http = require("http");
 const socketIO = require("socket.io");
+const fs = require("fs");
 
 const app = express();
 const server = http.createServer(app);
@@ -11,9 +12,56 @@ const usuariosConectados = {};
 io.on("connection", (socket) => {
   console.log("Usuario conectado");
 
+  fs.readFile("users.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    let connectedUsers;
+
+    try {
+      connectedUsers = JSON.parse(data);
+    } catch (error) {
+      console.error("Error al analizar los datos JSON:", error);
+      // Maneja el error de análisis como consideres adecuado
+    }
+        usuariosConectados[socket.id] = connectedUsers[socket.id] || "";
+
+    socket.emit("mensaje", {
+      autor: "Servidor",
+      mensaje: `¡Bienvenido al chat, ${usuariosConectados[socket.id]}!`
+    });
+    socket.broadcast.emit("mensaje", {
+      autor: "Servidor",
+      mensaje: `${usuariosConectados[socket.id]} se ha unido al chat`
+    });
+
+    io.emit("usuarios-conectados", Object.values(connectedUsers));
+  });
+
   socket.on("unirse-al-chat", (nombre) => {
     console.log(`El usuario ${nombre} se ha unido al chat`);
     usuariosConectados[socket.id] = nombre;
+
+    fs.readFile("users.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      const connectedUsers = JSON.parse(data);
+      connectedUsers[socket.id] = nombre;
+
+      fs.writeFile("users.json", JSON.stringify(connectedUsers), (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        console.log("El archivo users.json se ha actualizado");
+      });
+    });
 
     socket.emit("mensaje", {
       autor: "Servidor",
@@ -45,6 +93,25 @@ io.on("connection", (socket) => {
     });
 
     delete usuariosConectados[socket.id];
+
+    fs.readFile("users.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      const connectedUsers = JSON.parse(data);
+      delete connectedUsers[socket.id];
+
+      fs.writeFile("users.json", JSON.stringify(connectedUsers), (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        console.log("El archivo users.json se ha actualizado");
+      });
+    });
   });
 });
 
